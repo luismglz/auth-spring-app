81b2d2d642c2ae0a6f603dcb7bcf33eedbffa4f9
Luis GL <lmgl887@gmail.com>
2023-10-19 11:34:19 -0600

    add: empty project

---
+++ src/main/java/com/ruisu/authspringapp/AuthSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.authspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruisu/authspringapp/AuthSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruisu.authspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
